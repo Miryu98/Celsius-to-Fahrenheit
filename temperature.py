6d67c9b5fd2f38c06f9064ea355d40e3d795d371
Miłosz Salwa <75585351+Miryu98@users.noreply.github.com>
2023-04-18 15:16:59 +0200

    Create temperature.py

---
+++ temperature.py
@@ -0,0 +1,9 @@
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+celsius = int(input())
+fahrenheit = celsius_to_fahrenheit(celsius)
+print(fahrenheit)
+
+# Write a function to take the Celsius value as an argument and return the corresponding Fahrenheit value.
